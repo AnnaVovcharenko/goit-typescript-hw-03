5c4609b394ac0dee97bdb3efea8582020740c040
Vovcharenko Anna <129333985+AnnaVovcharenko@users.noreply.github.com>
2023-11-28 23:02:42 +0200

    1

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {
+    this.key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+  constructor(key: Key){
+    this.door = false;
+    this.key = key;
+  };
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person)
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if(this.key.getSignature() === key.getSignature()){
+            this.door = true;
+        }
+
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +49,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
